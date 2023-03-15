70a9a489795c2a90423afad724093f2849891b1e
RussianBoy64 <kozlachkovvd@gmail.com>
2023-03-15 23:57:39 +0400

    feat: install sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import './App.css';
+import styles from './App.module.scss';
 
-function App() {
-  return <div className="App">Hello World!</div>;
+class App extends React.Component {
+  render() {
+    return <div className={styles.App}>Hello World!</div>;
+  }
 }
 
 export default App;
