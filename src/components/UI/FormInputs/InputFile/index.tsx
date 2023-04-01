538fcc3b0863347da78866d36aa26dd1364fbadd
RussianBoy64 <kozlachkovvd@gmail.com>
2023-04-02 03:50:49 +0400

    refactor: rewrite inputFile to FC with react-hook-form

--- src/components/UI/FormInputs/InputFile/index.tsx
+++ src/components/UI/FormInputs/InputFile/index.tsx
@@ -1,29 +1,27 @@
-import { Component, LegacyRef } from 'react';
-import { IInputProps } from '../FormInputs';
+import { InputProps } from '../FormInputs';
 
 import styles from './styles.module.scss';
 
-export default class InputFile extends Component<IInputProps> {
-  render() {
-    const { reference, inputType, isNotValid } = this.props;
+export default function InputFile({ name, register, error }: InputProps) {
+  const validation = {
+    required: 'Download your photo',
+  };
 
-    return (
-      <div className={styles.wrapper}>
-        <span className={styles.title}>{inputType}</span>
-        <label className={styles.label} htmlFor={inputType}>
-          <input
-            className={styles.input}
-            type="file"
-            name={inputType}
-            id={inputType}
-            ref={reference as LegacyRef<HTMLInputElement>}
-            accept="image/png, image/jpeg"
-            title="Download your photo"
-          />
-          <span className={styles.download}>Download photo</span>
-        </label>
-        {isNotValid && <span className={styles.error}>Download your photo</span>}
-      </div>
-    );
-  }
+  return (
+    <div className={styles.wrapper}>
+      <span className={styles.title}>{name}</span>
+      <label className={styles.label} htmlFor={name}>
+        <input
+          className={styles.input}
+          type="file"
+          id={name}
+          {...register(name, validation)}
+          accept="image/png, image/jpeg"
+          title="Download your photo"
+        />
+        <span className={styles.download}>Download photo</span>
+      </label>
+      {error.photo && <span className={styles.error}>{error.photo.message}</span>}
+    </div>
+  );
 }
