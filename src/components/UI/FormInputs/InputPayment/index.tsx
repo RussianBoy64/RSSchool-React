305c0000fd7d939ed203669b52c2547c8e74baf2
RussianBoy64 <kozlachkovvd@gmail.com>
2023-04-02 03:46:12 +0400

    refactor: rewrite inputPayment to FC with react-hook-form

--- src/components/UI/FormInputs/InputPayment/index.tsx
+++ src/components/UI/FormInputs/InputPayment/index.tsx
@@ -1,5 +1,4 @@
-import { Component, LegacyRef } from 'react';
-import { IInputProps } from '../FormInputs';
+import { InputProps } from '../FormInputs';
 
 import styles from './styles.module.scss';
 
@@ -8,40 +7,36 @@ enum Payment {
   card = 'Card',
 }
 
-export default class InputPayment extends Component<IInputProps> {
-  render() {
-    const { reference, inputType, isNotValid } = this.props;
-    const labelStyles = isNotValid ? styles.label_invalid : '';
+export default function InputPayment({ name, register, error }: InputProps) {
+  const labelStyles = error.pay ? styles.label_invalid : '';
+  const validation = {
+    required: 'Choose payment type',
+  };
 
-    return (
-      <div className={styles.wrapper}>
-        <span className={styles.title}>{inputType}</span>
-        <div className={styles.inputContaiter}>
-          <label className={labelStyles} htmlFor={Payment.cash}>
-            {Payment.cash}
-          </label>
-          <input
-            className={styles.input}
-            type="radio"
-            name={inputType}
-            id={Payment.cash}
-            value={Payment.cash}
-            ref={reference as LegacyRef<HTMLInputElement>}
-          />
-          <label className={labelStyles} htmlFor={Payment.card}>
-            {Payment.card}
-          </label>
-          <input
-            className={styles.input}
-            type="radio"
-            name={inputType}
-            id={Payment.card}
-            value={Payment.card}
-            ref={reference as LegacyRef<HTMLInputElement>}
-          />
-        </div>
-        {isNotValid && <span className={styles.error}>Choose payment type</span>}
+  return (
+    <div className={styles.wrapper}>
+      <span className={styles.title}>{name}</span>
+      <div className={styles.inputContaiter}>
+        <label className={labelStyles} htmlFor={Payment.cash}>
+          {Payment.cash}
+        </label>
+        <input
+          className={styles.input}
+          type="radio"
+          {...register(name, validation)}
+          value={Payment.cash}
+        />
+        <label className={labelStyles} htmlFor={Payment.card}>
+          {Payment.card}
+        </label>
+        <input
+          className={styles.input}
+          type="radio"
+          {...register(name, validation)}
+          value={Payment.card}
+        />
       </div>
-    );
-  }
+      {error.pay && <span className={styles.error}>{error.pay.message}</span>}
+    </div>
+  );
 }
