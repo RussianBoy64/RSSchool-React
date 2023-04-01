922a9cf9331a5f7e8823bc46ebe1edf12a6f8050
RussianBoy64 <kozlachkovvd@gmail.com>
2023-04-02 03:02:12 +0400

    refactor: rewrite inputName to FC with react-hook-form

--- src/components/UI/FormInputs/InputName/index.tsx
+++ src/components/UI/FormInputs/InputName/index.tsx
@@ -1,32 +1,29 @@
-import { Component, LegacyRef } from 'react';
-import { IInputProps } from '../FormInputs';
+import { InputProps } from '../FormInputs';
 
 import styles from './styles.module.scss';
 
-export default class InputName extends Component<IInputProps> {
-  render() {
-    const { reference, inputType, isNotValid } = this.props;
-    const inputStyles = [styles.input];
-    if (isNotValid) inputStyles.push(styles.input_invalid);
+export default function InputName({ name, register, error }: InputProps) {
+  const inputStyles = [styles.input];
+  const validation = {
+    required: 'Please enter your name',
+    minLength: { value: 3, message: 'Name must contain 3 characters or more' },
+    maxLength: { value: 16, message: 'Name must be shorter than 15 characters' },
+    pattern: { value: /^[A-ZА-Я][a-zа-я]?/, message: 'The name must start with a capital letter' },
+  };
 
-    return (
-      <label className={styles.label}>
-        <span className={styles.title}>{inputType}</span>
-        <input
-          className={inputStyles.join(' ')}
-          type="text"
-          name={inputType}
-          id={inputType}
-          ref={reference as LegacyRef<HTMLInputElement>}
-          placeholder="Enter your name"
-          title="Name must start with a capital letter and not contain numbers, min 3 max 15 characters"
-        />
-        {isNotValid && (
-          <span className={styles.error}>
-            Name must start with a capital letter and not contain numbers, min 3 max 15 characters
-          </span>
-        )}
-      </label>
-    );
-  }
+  if (error.name) inputStyles.push(styles.input_invalid);
+
+  return (
+    <label className={styles.label}>
+      <span className={styles.title}>{name}</span>
+      <input
+        className={inputStyles.join(' ')}
+        type="text"
+        {...register(name, validation)}
+        placeholder="Enter your name"
+        title="Name must start with a capital letter and not contain numbers, min 3 max 15 characters"
+      />
+      {error.name && <span className={styles.error}>{error.name.message}</span>}
+    </label>
+  );
 }
