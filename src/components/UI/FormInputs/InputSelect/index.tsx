ff9cda825be3ad4528ce807d35319609a5a241a3
RussianBoy64 <kozlachkovvd@gmail.com>
2023-04-02 03:42:39 +0400

    refactor: rewrite inputSelect to FC with react-hook-form

--- src/components/UI/FormInputs/InputSelect/index.tsx
+++ src/components/UI/FormInputs/InputSelect/index.tsx
@@ -1,40 +1,33 @@
-import { Component, LegacyRef } from 'react';
-import { IInputProps } from '../FormInputs';
+import { InputProps } from '../FormInputs';
 
 import styles from './styles.module.scss';
 
 export enum Package {
-  info = 'Select packaging',
   without = 'Without packaging',
   standart = 'Standalone packing',
   gift = 'Gift packaging',
 }
 
-export default class InputSelect extends Component<IInputProps> {
-  render() {
-    const { reference, inputType, isNotValid } = this.props;
-    const inputStyles = [styles.input];
-    if (isNotValid) inputStyles.push(styles.input_invalid);
+export default function InputSelect({ name, register, error }: InputProps) {
+  const inputStyles = [styles.input];
+  const validation = {
+    required: 'Select packaging',
+  };
 
-    return (
-      <label className={styles.label}>
-        <span className={styles.title}>{inputType}</span>
-        <select
-          className={inputStyles.join(' ')}
-          name={inputType}
-          id={inputType}
-          ref={reference as LegacyRef<HTMLSelectElement>}
-          defaultValue={Package.info}
-        >
-          <option disabled value={Package.info}>
-            {Package.info}
-          </option>
-          <option value={Package.without}>{Package.without}</option>
-          <option value={Package.standart}>{Package.standart}</option>
-          <option value={Package.gift}>{Package.gift}</option>
-        </select>
-        {isNotValid && <span className={styles.error}>Select packaging</span>}
-      </label>
-    );
-  }
+  if (error.package) inputStyles.push(styles.input_invalid);
+
+  return (
+    <label className={styles.label}>
+      <span className={styles.title}>{name}</span>
+      <select className={inputStyles.join(' ')} {...register(name, validation)} defaultValue={''}>
+        <option disabled value={''}>
+          Select packaging
+        </option>
+        <option value={Package.without}>{Package.without}</option>
+        <option value={Package.standart}>{Package.standart}</option>
+        <option value={Package.gift}>{Package.gift}</option>
+      </select>
+      {error.package && <span className={styles.error}>{error.package.message}</span>}
+    </label>
+  );
 }
