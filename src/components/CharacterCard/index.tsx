aa1e1384aae901a31cf2964faca3a1fab33477d4
RussianBoy64 <kozlachkovvd@gmail.com>
2023-04-06 01:38:04 +0400

    feat: change ProductCard to CharacterCard

---
+++ src/components/CharacterCard/index.tsx
@@ -0,0 +1,30 @@
+// import { useState } from 'react';
+import { ICharacter } from 'helpers/fetchCharacters';
+
+import styles from './styles.module.scss';
+
+interface ICharacterCardProps {
+  character: ICharacter;
+}
+
+export default function CharacterCard({ character }: ICharacterCardProps) {
+  const {
+    images: { jpg },
+    name,
+    favorites,
+  } = character;
+
+  return (
+    <div className={styles.characterCard}>
+      <img src={jpg.image_url} alt={`${name} image`} />
+      <div className={styles.description}>
+        <span className={styles.description__name}>{name}</span>
+
+        <span className={styles.description__favorites}>
+          <i className={`fa-solid fa-heart ${styles.description__heart}`} />
+          {favorites}
+        </span>
+      </div>
+    </div>
+  );
+}
